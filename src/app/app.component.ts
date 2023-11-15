6b167c6ced641af70504cc0749ef51023917e1e6
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-11-15 13:54:56 +0100

    Promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import {Person} from './components/model/person.model';
 import {User} from './components/model/user.model';
 import {Student} from './components/model/student.model';
 
+interface Counter {
+  name: string,
+  value: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,4 +15,20 @@ import {Student} from './components/model/student.model';
 })
 export class AppComponent {
 
+
+  count!: Counter
+
+  constructor() {
+
+    console.log("1")
+    const myPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({name: 'mycounter', value: 10});
+      }, 3000);
+    });
+
+    myPromise.then(res => this.count = (res as Counter))
+    console.log("3")
+
+  }
 }
