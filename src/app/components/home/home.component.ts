ed49f2dbd79be602799ff272037a8563bac29e95
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-11-15 15:33:42 +0100

    'Fun' with observable

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,77 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {CountService} from '../../count.service';
+import {
+  from,
+  map,
+  zip,
+  filter,
+  interval,
+  Observable,
+  Subscription,
+  take,
+  takeWhile,
+  fromEvent,
+  audit,
+  auditTime
+} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
+
+
+  $myObv!: Observable<number>
+  subscription: Subscription | undefined
+
+  alive = true;
+
+  @ViewChild('mydiv') mydiv!: ElementRef;
+
+  data = ['Logan', 'Kiran', 'Zach', 'Dan', 'Emily']
+
 
   constructor(public cs: CountService) {
+  }
+
+  ngOnInit(): void {
+
+      /*return*/
+
+
+    this.$myObv = interval(200)
+      .pipe(
+        filter(v => v % 2 == 0),
+        map(v => {
+
+          console.log('Hello')
+          return v * 10
+        }),
+        takeWhile(v => this.alive),
+        take(5)
+      )
+  }
+  ngAfterViewInit(): void {
+    fromEvent<MouseEvent>(this.mydiv.nativeElement,"click")
+      .pipe(
+        map((e: MouseEvent) =>  ({x: e.clientX, y: e.clientY})),
+        auditTime(1000))
+      .subscribe(v => console.log(v))
+  }
+
+
+  startThatObs() {
+    this.$myObv.subscribe({
+      next: console.log,
+      error:  e =>  console.error(e),
+      complete: () => console.log("AM DONE")
+    })}
+
+
+  ngOnDestroy(): void {
+    this.alive = false
   }
 
 }
--- src/app/count.service.ts
+++ src/app/count.service.ts
@@ -6,7 +6,5 @@ import { Injectable } from '@angular/core';
 export class CountService {
 
   title = 'My awesome counter'
-
-
   constructor() { }
 }
