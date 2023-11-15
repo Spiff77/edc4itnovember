dbc220b0e84ac569d18016801bdd519a81726011
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-11-15 20:55:54 +0100

    rxjs mergeMap with httpCLient (2 queries)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Person} from './model/person.model';
 import {User} from './model/user.model';
 import {Student} from './model/student.model';
+import {HttpClient} from '@angular/common/http';
+import {map, mergeMap, switchMap} from 'rxjs';
 
 interface Counter {
   name: string,
@@ -13,12 +15,36 @@ interface Counter {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+
+  arr = [6,7,3,4]
 
   count!: Counter
 
-  constructor() {
+  ngOnInit(): void {
+    this.http.get<any>('https://jsonplaceholder.typicode.com/posts/11')
+      .pipe(
+        switchMap(v => {
+          const post = v;
+          return this.http.get<any>(`https://jsonplaceholder.typicode.com/users/${v['userId']}`)
+            .pipe(map(w => {
+              post['user']=w
+              delete post['userId']
+              return post
+            }))
+        } )
+      ).subscribe(v => console.log("FINAL", v))
+  }
+
+  constructor(private http: HttpClient) {
+
+
+    this.arr.map((sdgsdfg, index) => {
+      console.log(index, sdgsdfg)
+      return sdgsdfg * 10
+    })
+
 
     console.log("1")
     const myPromise = new Promise((resolve, reject) => {
